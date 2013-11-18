7477151980125122b9a1a26975f414ba192917ec
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-11-18 09:16:46 -0600

    Incluidos archivos de devise en español. Cambios en etiquetas 'Usuario' para el panel administrativo

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register AdminUser do     
+ActiveAdmin.register AdminUser, as: "Usuario" do     
   index do                            
     column :email                     
     column :current_sign_in_at        
